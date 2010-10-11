a44b810559451bbbbeed115185af195b6d62fcd3
Krzysztof Kotowicz <kkotowicz@gmail.com>
2010-10-11 16:07:49 +0200

    - ipv6 compatibility

--- e4x/victim/schema.sql
+++ e4x/victim/schema.sql
@@ -1,2 +1,3 @@
-CREATE TABLE comments(id integer primary key, author varchar(20), ip varchar(15), comment text);
+CREATE TABLE comments (id integer primary key, author varchar(20), ip varchar(39), comment text);
 CREATE INDEX comments_ip on comments (ip);
+
