9597ee8b977b599ec122eea57fa82f6c020026af
Krzysztof Kotowicz <kkotowicz@gmail.com>
2011-04-14 15:02:44 +0200

    - limit on file size

--- webkit-upload/ProgressBar.js
+++ webkit-upload/ProgressBar.js
@@ -4,7 +4,7 @@ ProgressBar.prototype.start = function() {
     $("#progress").show();
     $('#p').animate({left: '500px'}, 10000, 'linear',ProgressBar.prototype.reset);
 }
-ProgressBar.prototype.reset = function() {
+ProgressBar.prototype.reset = function() { 
 	$('#p').css('left',  '-100px');
 	setTimeout(ProgressBar.prototype.start, 100);
 }
