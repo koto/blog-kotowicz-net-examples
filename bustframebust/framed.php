f9e78b22450070e2bebb8b389af07b58cbfa4d75
Krzysztof Kotowicz <kkotowicz@gmail.com>
2011-09-03 20:54:40 +0200

    - new vectors, new launching

--- bustframebust/framed.php
+++ bustframebust/framed.php
@@ -1,23 +1,55 @@
 <html>
 <head>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script src="jquery.min.js"></script>
 </head>
 <body>
 hello i will framebust in a moment.
 <script>
+<?php
 
+$pre = array(
+    'functionvar' => <<<EOF
+	var framebust = function() {
+	    document.write("you lost");
+//	    alert('you lost');
+	    location = 'data:text/html,you%20lost';
+	}
+EOF
+);
 
-var framebust = function() {
-       self.location = 'data:text/html,you%20lost';
-       console.log('framebusting');
-}
+$codes = array(
+    'topself' => <<<EOF
+	if (top != self) {
+	    top.location = self.location;
+	}
+EOF
+    ,'locationhref' => <<<EOF
+	location.href = 'data:text/html,you%20lost';
+EOF
+    ,'functionvar' => <<<EOF
+	framebust();
+EOF
+);
+
+$framebust = '';
+$framebust_pre = '';
+
+if (!empty($_GET['method']) && array_key_exists($_GET['method'], $codes)) {
+    $m = $_GET['method'];
+    $framebust = $codes[$m];
+    $framebust_pre = !empty($pre[$m]) ? $pre[$m] : '';
+} 
+
+?>
+
+<?php echo $framebust_pre ?>
 
 setTimeout(function() {
 
-	framebust();
+        <?php echo $framebust ?>
 	
 	setTimeout(function() {
-	    document.body.innerHTML += ('you won!');
+	    document.body.innerHTML += ('you won. unless im top, in that case - no, you didn\'t!');
 	},300);
 	
 	$('body').click(function(){ 
@@ -25,6 +57,7 @@ setTimeout(function() {
 	});
 
 }, 500);
+
 </script>
 </body>
 </html>
--- bustframebust/index.php
+++ bustframebust/index.php
@@ -1,3 +1,4 @@
+<head>
 <style>
 dt {
     font-weight: bold;
@@ -13,37 +14,72 @@ span {
 }
 
 iframe {
-    width: 300px;
+    width: 400px;
     height: 60px;
+    display: block;
 }
 
+a {display: block}
+
+h2 a {display: inline;}
 dd {
     position: relative;
 }
 
 </style>
+<script src="jquery.min.js"></script>
+</head>
+<body>
 <h1>Same-domain ES5 iframe anti-frame-busting</h1>
 <h2>By <a href="http://blog.kotowicz.net">Krzysztof Kotowicz</a></h2>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-<iframe id=f></iframe>
 <script>
-    var f = $("#f")[0], // frame
-        w = f.contentWindow, // window
+var overload = function(f,method) {
+    var w = f.contentWindow, // window
         d = f.contentDocument || f.contentWindow.document; // document
-     
-    $(f).load(function() {
-
+    $(f).load(function() { // this one is too late, after js already executed in frame
     });
 
-    if (Object.defineProperty) {
-    Object.defineProperty(d, "location",{ writable: false, configurable: false}); // works in ie9+
-    Object.defineProperty(w, "location",{ writable: false, configurable: false}); // works in firefox, ie9+
-    Object.defineProperty(w, "framebust",{ configurable: true, set: function(v) {}, get: function() {
-        return function() { console.log('im hijacked') };
-    }});
-    }
+    if (Object.defineProperty) { // ie9+, webkit
     
-    f.src = 'framed.php';
-    
+        Object.defineProperty(d, "location",{ writable: false, configurable: false}); // works in ie9+, cant confirm IE
+         
+        // beats location=URL assignment (which would work even in iframe sandbox)
+	Object.defineProperty(w, "location",{ writable: false, configurable: false, value: { reload: function () {}, href: "" }}); // works in firefox same-domain, ie9+, cant confirm IE
 
+	// beats top.location=self.location
+        Object.defineProperty(window, "location", {writable: false});  // beats top.location = self.location, ff
+
+        // beats whole framebust() function call
+	Object.defineProperty(w, "framebust",{ configurable: false, set: function(v) {}, get: function() { // firefox
+		return function() { console.log('im hijacked') };
+        }});
+        // function constructor overload?
+        
+    } else {
+       // opera, ie <=8
+       //alert('no support for Object.defineProperty');
+    }
+
+    var base_url = 'framed.php?method=' + method;
+//    f.src = location.href.replace('attacker','victim').replace(/\/[^/]*$/,'/framed.php');
+    // apparently you need to be fast here, no delays, maybe about:blank loading interferes
+    f.src = base_url;    
+};
+
+var methods = [
+    'topself', 
+    'locationhref', 
+    'functionvar']
+    , iframe
+    , baseurl
+    , launcher = $('<a href=#>').click(function() { 
+    $("<iframe>").insertAfter(this);
+    overload(this.nextSibling,this.rel);
+    return false; 
+});
+
+for (var i=0; i<methods.length; i++) {
+    launcher.clone(true).attr('rel', methods[i]).text(methods[i]).appendTo('body');
+}
 </script>
+</body>
