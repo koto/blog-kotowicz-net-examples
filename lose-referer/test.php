cac544a4669e53c4a34865c9f222d9349ab2402d
Krzysztof Kotowicz <kkotowicz@gmail.com>
2011-10-24 16:50:42 +0200

    - better ui

--- lose-referer/test.php
+++ lose-referer/test.php
@@ -8,7 +8,6 @@ header('Content-Type: text/html;charset=utf-8');
 if <code>Referer</code> is from a third-party site, the request processing is blocked. However, loose referer checking allows requests with no referrer header at all.
 <p>There are various ways to make a request losing the <code>Referer</code> header - most common is just doing a 30x redirect either through attacker's site or a third party
 URL shortening service like <a href="//goo.gl">goo.gl</a>. I've tried to find ways how to do it <strong>client-side only</strong>, in Javascript. Here are the results.
-<h2>Test cases</h2>
 
 <script>
 var url = 'http://victim.kotowicz.net/lose-referer/test.php?text=hello';
@@ -33,6 +32,8 @@ function post_and_lose() {
 
 </script>
 <?php if (!empty($_GET['text'])) : ?>
+<h2>Result</h2>
+<div style="border: 1px dashed black; padding: 0.5em">
 <?php
 if (!empty($_SERVER['HTTP_REFERER'])) {
 	echo '<p><font color=red>Boo: your referer is ' . htmlspecialchars($_SERVER['HTTP_REFERER']) . '</font></p>'; 
@@ -44,18 +45,20 @@ echo "<p>Method: <strong>" . htmlspecialchars($_SERVER['REQUEST_METHOD']) . '</s
 ?>
 
 <p><?php if (!empty($_REQUEST['text'])) echo htmlspecialchars($_REQUEST['text']); ?>
-<?php endif; ?>
+</div>
+<p><a href="//attacker.kotowicz.net/lose-referer/test.php">Return to other test cases</a></p>
+<?php else: ?>
 <p>Standard link: <a href="//victim.kotowicz.net/lose-referer/test.php?text=hello">normal</a>
 <h3>Bypasses:</h3>
 <h4>GET requests</h4>
 <ul>
-<li><a href="#" onclick="lose_referer()">data: with location=</a> - Chrome
+<li><a href="#" onclick="lose_referer()">data: with location=</a> - Chrome / Safari
 <li><a href="#" onclick="lose_in_ie();">window.open()</a> - MSIE
-<li><a href="#" onclick="lose_in_ff();">data: with meta refresh</a> - Firefox / Opera / Chrome
+<li><a href="#" onclick="lose_in_ff();">data: with meta refresh</a> - Firefox / Opera / Chrome / Safari
 </ul>
 <h4>POST requests</h4>
 <ul>
-<li><a href="#" onclick="post_and_lose()">data: with meta refresh &amp; form submit</a> - Firefox / Chrome
+<li><a href="#" onclick="post_and_lose()">data: with meta refresh &amp; form submit</a> - Firefox / Chrome / Safari
 </ul>
-
-<p>Ideas welcome! Mail me at kkotowicz (it's a gmail account)
+<?php endif; ?>
+<p>Ideas welcome! Mail me at kkotowicz (it's a gmail account) or reach me on Twitter (@kkotowicz)
